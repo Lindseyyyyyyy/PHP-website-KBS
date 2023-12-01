e8d9319ab5355758886d0c8f619495a2175443c0
31637 <julieabigailv@gmail.com>
2023-12-01 15:54:07 +0100

    database connectie voor productpagina

--- productpagina.php
+++ productpagina.php
@@ -1,6 +1,19 @@
 <?php
+
+$servername = "localhost"; // Replace with your server name
+$username = "root"; // Replace with your username
+$password = ""; // Replace with your password
+$dbname = "test"; // Replace with your database name
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 $query = "SELECT * FROM product";
-$result = $db->query($query);
+$result = $conn->query($query);
 
 ?>
 <html>
