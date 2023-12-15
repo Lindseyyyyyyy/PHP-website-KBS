dd19b3159b0c19781414a06e57832495f52d498b
31637 <julieabigailv@gmail.com>
2023-12-15 03:05:03 +0100

    dropdown filters werken

--- productpagina.php
+++ productpagina.php
@@ -14,7 +14,15 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 $query = "SELECT * FROM product ";
-$result = $conn->query($query);
+$sorteer = "SELECT * FROM product ORDER BY price";
+$sorteerhoog= "SELECT * FROM product ORDER BY price DESC ";
+
+$populair= "SELECT product_id, SUM(quantity), P.*
+FROM order_item as O
+JOIN product as P on P.id = O.product_id
+GROUP BY product_id
+ORDER BY SUM(quantity) DESC";
+
 
 ?>
 
@@ -62,10 +70,18 @@ $result = $conn->query($query);
           <p class="lead">Explore our amazing gadgets.</p>
 
                       <!-- productfilters -->
+              <?php
+              $productsoort = "laptops";
+              $prijsklasse = "laagste";
+              $sort = "laagsteprijs";
+
+
+              ?>
             <div class="container text-left">
                 <div style="background-color: #230536; color: #fff; padding: 20px; text-align: left;">
                     <p class="lead">productfilters.</p>
             <form>
+                <form method="GET" action="productpagina.php">
               categorie: <input type="checkbox" name="productsoort" value= "laptops" > laptops
               <input type="checkbox" name="productsoort" value="phones" > phones
                 <input type="checkbox" name="productsoort" value="opslag"> opslag
@@ -78,18 +94,20 @@ $result = $conn->query($query);
                 <input type="checkbox" name="prijsklasse" value="hoog" > boven de 250
                 <br>
 
-                producten sorteren op: <select name="sorteren op" id="sorteren" >
-                    <option value="nieuw">nieuwste eerst</option>
-                    <option value="laagsteprijs">prijs van laag naar hoog</option>
-                    <option value="hoogsteprijs">prijs van hoog naar laag</option>
-                    <option value="niet"> populair </option>
-                </select>
+                producten sorteren op: <select name="sort" id="sorteren" >
+                    <option value="cat">categorie</option>
+                    <option value="lprijs">prijs van laag naar hoog</option>
+                    <option value="hprijs">prijs van hoog naar laag</option>
+                        <option value="populair">populair</option>
+
+                </select> <form>
+
         </div>
       </div>
           </div>
   <div class="container text-right">
       <div style="background-color: #230536; color: #fff; padding: 20px; text-align: right;">
-          <input type="submit" name="submit" value="Keuze bevestigenn">
+          <input type="submit" name="submit" value="Keuze bevestigen">
           <br>
           <input type="reset" value="filters resetten">
       </div>
@@ -97,9 +115,28 @@ $result = $conn->query($query);
           </div>
     </div>
 
+  <?php
+  $sort = isset($_GET['sort']) ? $_GET['sort'] : '';
+
+  if ($sort == "lprijs"){
+      $query = $sorteer;
+  } else if($sort == "hprijs"){
+      $query = $sorteerhoog;
+  } else if($sort == "cat"){
+      $query = "SELECT * FROM product ORDER BY category";
+  } else if($sort == "abc") {
+      $query = "SELECT * FROM product ORDER BY name";
+  } else if($sort == "populair"){
+      $query = $populair;
+  } else {
+      $query = $populair;
+  }
+
+  ?>
   <!-- Product Grid -->
 
     <?php
+    $result = $conn->query($query);
     if ($result->num_rows > 0) {
         // Output data of each row
         while ($row = $result->fetch_assoc()) {
@@ -118,6 +155,8 @@ $result = $conn->query($query);
                 </div>
               </div>
             </div> 
+            </div>
+            
             </section>
             ';
         }
