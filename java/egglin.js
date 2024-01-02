55d1e4539d0efceaab424ec9ed6e42876e8e873c
lindsey <lindseykartodimedjo@gmail.com>
2024-01-02 20:46:37 +0100

    mn easteregg toegevoegd, als je klikt op nerdy gadgets vallen de letters uit elkaar

--- java/egglin.js
+++ java/egglin.js
@@ -1,13 +1,19 @@
-function changeBackgroundColor() {
-    var body = document.body;
-    body.style.backgroundColor = getRandomColor();
-}
+document.getElementById("nerdy-gadgets").addEventListener("click", function() {
+    var text = document.getElementById("nerdy-gadgets").innerText;
+    var letters = text.split("").map(function(char, index) {
+        return "<span id='letter-" + index + "'>" + char + "</span>";
+    });
 
-function getRandomColor() {
-    var letters = "0123456789ABCDEF";
-    var color = "#";
-    for (var i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-}
+    document.getElementById("nerdy-gadgets").innerHTML = letters.join("");
+
+    letters.forEach(function(_, index) {
+        var letterElement = document.getElementById("letter-" + index);
+        setTimeout(function() {
+            letterElement.style.transform = "translateY(300px) rotate(" + (Math.random() * 360) + "deg)";
+        }, index * 50);
+    });
+
+    setTimeout(function() {
+        document.getElementById("nerdy-gadgets").innerHTML = text;
+    }, 4000);
+});
